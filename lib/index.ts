6d678c3758b80f343ef9f38035a2ce9c33a828c4
jaxoncreed <jaxoncreed@gmail.com>
2022-09-29 15:54:32 -0400

    initial oidc work

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,19 @@
-console.log('test');
+import { Provider } from "oidc-provider";
+const configuration = {
+  // ... see /docs for available configuration
+  clients: [
+    {
+      client_id: "foo",
+      client_secret: "bar",
+      redirect_uris: ["https://oidcdebugger.com/debug"],
+    },
+  ],
+};
+
+const oidc = new Provider("http://localhost:3000", configuration);
+
+oidc.listen(3000, () => {
+  console.log(
+    "oidc-provider listening on port 3000, check http://localhost:3000/.well-known/openid-configuration"
+  );
+});
