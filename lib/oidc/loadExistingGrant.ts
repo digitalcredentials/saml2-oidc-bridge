1b98206399304e4b66ca0b21b71bd72ba498d0a7
Ubuntu <ubuntu@ip-172-31-85-15.ec2.internal>
2022-11-15 20:28:29 +0000

    Login works

---
+++ lib/oidc/loadExistingGrant.ts
@@ -0,0 +1,35 @@
+import { KoaContextWithOIDC } from "oidc-provider";
+
+export async function loadExistingGrant(ctx: KoaContextWithOIDC) {
+  if (!ctx.oidc.client?.clientId) {
+    return undefined;
+  }
+
+  const grantId =
+    (ctx.oidc.result &&
+      ctx.oidc.result.consent &&
+      ctx.oidc.result.consent.grantId) ||
+    ctx.oidc.session?.grantIdFor(ctx.oidc.client.clientId);
+
+  if (grantId) {
+    return ctx.oidc.provider.Grant.find(grantId);
+  }
+
+  const grant = new ctx.oidc.provider.Grant({
+    clientId: ctx.oidc.client.clientId,
+    accountId: ctx.oidc.session?.accountId,
+  });
+
+  const scopes = (ctx.oidc.params?.scope as string)?.split(" ") || "";
+
+  console.log("scopes");
+  console.log(scopes);
+  scopes.forEach((scope) => {
+    grant.addOIDCScope(scope);
+  });
+
+  // TODO: Use ttl.Grant
+  await grant.save(3600);
+
+  return grant;
+}
